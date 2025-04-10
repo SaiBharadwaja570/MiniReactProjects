78688dc24e50d53cd551ff46a30856ab825d781d
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2025-04-10 11:21:41 +0530

    Adding readme file

---
+++ CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useState, useuseEffect } from 'react'
+
+function useCurencyInfo(currency){
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`
+https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{currency}.json
+`)    
+.then(res => res.json())
+.then(res => setData[res[currency]])
+.catch(err => console.log(err))
+    }, [currency])
+    console.log(data);
+    return data;
+}
+
+export default useCurencyInfo;
