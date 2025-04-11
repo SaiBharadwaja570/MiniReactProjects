f38947fe23cf8472a3e88b2312002d3edc635111
SaiBharadwaja570 <saibharadwaja1906@gmail.com>
2025-04-11 14:38:54 +0530

    Updated app-with-react-router

---
+++ app-with-react-router/src/components/GitHub/Github.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom';
+
+const Github = () => {
+    const data = useLoaderData();
+    // const [data, setData] = useState([]);
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/SaiBharadwaja570')
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    //     .catch(e=>console.log(e))
+    // }, [])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white text-4xl'>Github followers: {data.followers}
+    <img  src={data.avatar_url} alt="Git Picture" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+
+export const githubInfoLoader = async () => {
+    const res = await fetch('https://api.github.com/users/SaiBharadwaja570')
+    return res.json()
+  }
---
+++ app-with-react-router/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const User = () => {
+const {userid} = useParams();
+
+return (
+    <div className='bg-green-600 text-white'>User: {userid}</div>
+  )
+}
+
+export default User
